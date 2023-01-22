c4caa2ef5b5ecb616d77d8edef4c204be70d4205
Ethan Netz <ethannetz@hotmail.com>
2023-01-22 01:07:38 -0500

    add prettier

--- app/src/lib/stores/auth.ts
+++ app/src/lib/stores/auth.ts
@@ -1,59 +1,56 @@
-import type { Auth, } from "firebase/auth";
+import type { Auth } from 'firebase/auth';
 import type { FirebaseApp } from 'firebase/app';
 import { dev } from '$app/environment';
 import { derived, type Readable } from 'svelte/store';
 import { app } from '$lib/stores';
 
 const createAuth = () => {
-    let auth: Auth;
+	let auth: Auth;
 
-    const { subscribe } = derived<Readable<FirebaseApp>, Auth>(
-        app,
-        ($app, set) => {
-            async function init() {
-                /** Firebase app is not ready yet */
-                if (!$app)
-                    return;
-                /** Auth is already created, no reason to initialize */
-                if (auth)
-                    return;
+	const { subscribe } = derived<Readable<FirebaseApp>, Auth>(app, ($app, set) => {
+		async function init() {
+			/** Firebase app is not ready yet */
+			if (!$app) return;
+			/** Auth is already created, no reason to initialize */
+			if (auth) return;
 
-                const { getAuth, connectAuthEmulator } = await import('firebase/auth');
-                auth = getAuth($app);
-                if (dev) {
-                    connectAuthEmulator(auth, 'http://localhost:9099');
-                }
-                set(auth);
-            }
+			const { getAuth, connectAuthEmulator } = await import('firebase/auth');
+			auth = getAuth($app);
+			if (dev) {
+				connectAuthEmulator(auth, 'http://localhost:9099');
+			}
+			set(auth);
+		}
 
-            init();
-        }
-    );
+		init();
+	});
 
-    async function providerFor(name: string) {
-        const { GoogleAuthProvider } = await import('firebase/auth');
-        switch (name) {
-            case 'google': return new GoogleAuthProvider();
-            default: throw 'unknown provider ' + name;
-        }
-    }
+	async function providerFor(name: string) {
+		const { GoogleAuthProvider } = await import('firebase/auth');
+		switch (name) {
+			case 'google':
+				return new GoogleAuthProvider();
+			default:
+				throw 'unknown provider ' + name;
+		}
+	}
 
-    async function signInWith(name: string) {
-        const { signInWithRedirect } = await import('firebase/auth');
-        const provider = await providerFor(name);
-        await signInWithRedirect(auth, provider);
-    }
+	async function signInWith(name: string) {
+		const { signInWithRedirect } = await import('firebase/auth');
+		const provider = await providerFor(name);
+		await signInWithRedirect(auth, provider);
+	}
 
-    async function signOut() {
-        const { signOut } = await import('firebase/auth');
-        await signOut(auth);
-    }
+	async function signOut() {
+		const { signOut } = await import('firebase/auth');
+		await signOut(auth);
+	}
 
-    return {
-        subscribe,
-        signInWith,
-        signOut,
-    };
+	return {
+		subscribe,
+		signInWith,
+		signOut
+	};
 };
 
 export const auth = createAuth();
--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,25 +1,25 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
 admin.initializeApp(functions.config().firebase);
 
 export const sayHello = functions.https.onCall((data, context) => {
-    return { message: 'Hello from the emulator' };
+	return { message: 'Hello from the emulator' };
 });
 
-// Firebase function 
+// Firebase function
 export const createAccountDocument = functions.auth.user().onCreate((user) => {
-    // get user data from the auth trigger
-    const userUid = user.uid; // The UID of the user.
-    //const email = user.email; // The email of the user.
-    //const displayName = user.displayName; // The display name of the user.
+	// get user data from the auth trigger
+	const userUid = user.uid; // The UID of the user.
+	//const email = user.email; // The email of the user.
+	//const displayName = user.displayName; // The display name of the user.
 
-    // set account  doc  
-    const account = {
-        useruid: userUid,
-        calendarEvents: []
-    };
-    functions.logger.info("Hello logs from auth!", { structuredData: true });
-    // write new doc to collection
-    return admin.firestore().collection('accounts').add(account);
+	// set account  doc
+	const account = {
+		useruid: userUid,
+		calendarEvents: [],
+	};
+	functions.logger.info('Hello logs from auth!', { structuredData: true });
+	// write new doc to collection
+	return admin.firestore().collection('accounts').add(account);
 });
